5928c975c785c7694607189cb547c2d85d0afdf4
Ibad Shah <iamibadd@gmail.com>
2022-10-06 16:01:55 +0500

    Subject: Interceptor modified.

--- src/interceptors/index.js
+++ src/interceptors/index.js
@@ -1,33 +1,25 @@
 import axios from 'axios';
 import {env} from '../config/environment-config';
 
-const service = axios.create({
+const api = axios.create({
     baseURL: env.API_ENDPOINT_URL,
     timeout: 60000
 });
 
 // API request interceptor
-service.interceptors.request.use(config => {
+api.interceptors.request.use(config => {
     return config;
 }, error => {
-    // Do something with request error here
+    // Handle request error here
     return Promise.reject(error);
 })
 
 // API response interceptor
-service.interceptors.response.use((response) => {
+api.interceptors.response.use((response) => {
     return response.data;
 }, (error) => {
-    let notificationParam = {
-        message: '',
-        duration: 3
-    }
-    if (error) {
-        notificationParam.message = 'Api Failure';
-        notificationParam.description = error?.response?.data?.message || error?.response?.data;
-    }
-    // Do something with request error here
-    return Promise.reject(error);
+    // Handle response error here
+    return Promise.reject(error?.response?.data?.message || error?.response?.data);
 });
 
-export default service;
+export default api;
